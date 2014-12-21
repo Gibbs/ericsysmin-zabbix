c2096b2d3d22c14e1c0f6ad000d3459cc355b8ce
Dan Gibbs <daniel.gibbs@gmail.com>
2014-12-21 14:36:23 +0000

    Use distribution code name for Debian/Ubuntu repositories

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -10,7 +10,7 @@ class zabbix::repo::ubuntu {
 
   apt::source { 'zabbix':
     location   => "http://repo.zabbix.com/zabbix/${::zabbix::repo::version}/ubuntu/",
-    release    => 'precise',
+    release    => $::lsbdistcodename,
     repos      => 'main',
     key        => '79EA5ED4',
     key_source => 'http://repo.zabbix.com/zabbix-official-repo.key',
